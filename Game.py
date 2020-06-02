4d8e6108e823ae023616ed551659cffa3b809592
celidur <gaetansjo@gmail.com>
2020-06-02 13:13:46 +0200

    Update collision 2

--- Game.py
+++ Game.py
@@ -18,7 +18,7 @@ Map, Length, Width = import_map()
 pygame.init()
 pygame.display.set_caption("Game")
 Screen = pygame.display.set_mode((704, 736))
-x, y, velocity, pressed = 48 * 64, 30 * 64, 10, {}
+x, y, velocity, pressed = 48 * 64, 30 * 64, 8, {}
 player = Player()
 
 
@@ -37,6 +37,10 @@ def management_Screen(X1, X2, Y1, Y2, n, Block):
                     else:
                         Afficher_case(Block[Map[x // 64 + X_case][y // 64 + Y_case][n]], X_case, Y_case)
 
+def house(c):
+    if Map[((x + 32) // 64)][((y + 32) // 64)][4] != c and Map[((x + 32) // 64)][((y + 32) // 64) - 1][4] != c:
+        return True
+    return False
 
 def Afficher():
     global Width, Length, x, y
@@ -44,7 +48,7 @@ def Afficher():
     management_Screen(-5, 3, -6, 8, 0, block)
     management_Screen(-5, 3, -6, 8, 1, block)
     management_Screen(-5, 3, -6, 8, 2, block)
-    if Map[((x + 32) // 64)][((y + 32) // 64)][4] != "h1" and Map[((x + 32) // 64)][((y + 32) // 64) - 1][4] != "h1":
+    if house('h1') and house('h2') and house('h3'):
         Screen.blit(player.image, (11 // 2 * 64, 5 * 64))
         management_Screen(-9, 8, -8, 13, 4, block2)
     else:
@@ -53,26 +57,42 @@ def Afficher():
     pygame.display.flip()
 
 
+def Collision(a, b, c, d, e, f, g, h, i, j, k):
+    if (a // 32) % 2 == 0:
+        b, c, d, e = b // 64, c // 64, d // 64, e // 64
+        if (not Map[b][d][3][f]) and (not Map[c][e][3][g]):
+            return True
+    else:
+        b, c, d, e = (b + j) // 64, (c + j) // 64, (d + k) // 64, (e + k) // 64
+        if (not Map[b][d][3][h]) and (not Map[c][e][3][i]):
+            return True
+    return False
+
+
 def Keyboard_pressed(Pressed):
     global x, y, velocity
     move = False
     if Pressed.get(pygame.K_DOWN) and y // 64 < Width - 1:
-        if not Map[((x + 32) // 64)][(y // 64) + 1][3]:
+        d = Collision(y + 14, x + 17, x + 50, y + 50, y + 50, 1, 0, 3, 2, 0, -32)
+        if d:
             y += velocity
             player.Move("down")
             move = True
     elif Pressed.get(pygame.K_UP) and (y + 63) // 64 > 0:
-        if not Map[((x + 32) // 64)][(y // 64)][3]:
+        d = Collision(y + 16, x + 17, x + 50, y + 4, y + 4, 3, 2, 1, 0, 0, +32)
+        if d:
             y -= velocity
             player.Move("up")
             move = True
     if Pressed.get(pygame.K_RIGHT) and x // 64 < Length - 1:
-        if not Map[(x // 64) + 1][(y + 32) // 64][3]:
+        d = Collision(x + 14, x + 56, x + 56, y + 42, y + 10, 0, 2, 1, 3, -32, 0)
+        if d:
             x += velocity
             if not move:
                 player.Move("right")
     elif Pressed.get(pygame.K_LEFT) and (x + 63) // 64 > 0:
-        if not Map[(x // 64)][(y + 32) // 64][3]:
+        d = Collision(x + 16, x + 10, x + 10, y + 42, y + 10, 1, 3, 0, 2, +32, 0)
+        if d:
             x -= velocity
             if not move:
                 player.Move("left")
