3577b0ce834449b376b05c6d3709ba65020f7ae0
celidur <gaetansjo@gmail.com>
2020-07-07 21:07:37 +0200

    update

--- Display.py
+++ Display.py
@@ -12,6 +12,8 @@ class Display:
         self.size_window = size_window
         self.frame = time.time()
         self.background = background
+        self.ii = 0
+        self.i1 = time.time() + 1
 
     @staticmethod
     def Afficher_case(block, x_case, y_case, move_x=0, move_y=0):
@@ -19,17 +21,27 @@ class Display:
                          (int((x_case + 5) * 64 - (Game.x % 64)) + move_x,
                           int((y_case + 5) * 64 - (Game.y % 64) + move_y)))
 
-    def management_Screen(self, X1, X2, Y1, Y2, n, Block):
+    def Screen_update(self, n, X_case, Y_case):
+        try:
+            if n == 4:
+                block_2 = self.block2[Game.Map[Game.x // 64 + X_case][Game.y // 64 + Y_case][4]]
+                self.Afficher_case(block_2[0], X_case, Y_case, block_2[1], block_2[2])
+            else:
+                self.Afficher_case(self.block[Game.Map[Game.x // 64 + X_case][Game.y // 64 + Y_case][n]], X_case,
+                                   Y_case)
+        except KeyError:
+            pass
+
+    def management_Screen(self, X1, X2, Y1, Y2, n=0):
         for X_case in range(X1, (11 + 1) // 2 + X2):
             for Y_case in range(Y1, Y2):
                 if 0 <= X_case + Game.x // 64 < self.Length and Game.y // 64 + Y_case >= 0 and Y_case + Game.y // 64 < self.Width:
-                    if Game.Map[Game.x // 64 + X_case][Game.y // 64 + Y_case][n] in Block:
-                        if n == 4:
-                            block_2 = Block[Game.Map[Game.x // 64 + X_case][Game.y // 64 + Y_case][n]]
-                            self.Afficher_case(block_2[0], X_case, Y_case, block_2[1], block_2[2])
-                        else:
-                            self.Afficher_case(Block[Game.Map[Game.x // 64 + X_case][Game.y // 64 + Y_case][n]], X_case,
-                                               Y_case)
+                    if n == 4:
+                        self.Screen_update(4, X_case, Y_case)
+                    else:
+                        self.Screen_update(0, X_case, Y_case)
+                        self.Screen_update(1, X_case, Y_case)
+                        self.Screen_update(2, X_case, Y_case)
 
     @staticmethod
     def house(c):
@@ -40,14 +52,13 @@ class Display:
 
     def display_game(self):
         Game.Screen.blit(self.background, (0, 0))
-        for i in range(3):
-            self.management_Screen(-5, 3, -6, 8, i, self.block)
-        if self.house('h1') and self.house('h2') and self.house('h3'):
+        self.management_Screen(-5, 3, -6, 8)
+        if not self.house('h1') and self.house('h2') and self.house('h3'):
+            self.management_Screen(-9, 8, -8, 13, 4)
             Game.Screen.blit(Game.player.image, (11 // 2 * 64, 5 * 64))
-            self.management_Screen(-9, 8, -8, 13, 4, self.block2)
         else:
-            self.management_Screen(-9, 8, -8, 13, 4, self.block2)
             Game.Screen.blit(Game.player.image, (11 // 2 * 64, 5 * 64))
+            self.management_Screen(-9, 8, -8, 13, 4)
         if Game.menu == 0:
             Game.button_shop.display_button()
             Game.button_menu.display_button()
@@ -68,3 +79,8 @@ class Display:
             self.display_game()
             pygame.display.flip()
             self.frame = time.time() - 10
+            self.ii += 1
+        if time.time() > self.i1:
+            print(self.ii)
+            self.i1 = time.time() + 1
+            self.ii = 0
--- Game.py
+++ Game.py
@@ -1,30 +1,19 @@
 from Variable import *
 import time
-x, y, velocity, pressed, menu, escape = 48 * 64, 30 * 64, 8, {}, 0, time.time()
+ 
+x, y, menu, escape = 48 * 64, 30 * 64, 0, time.time()
 
 
 def save():
     pass
 
 
-def Collision(a, b, c, d, e, f, g, h, i, j, k):
-    if (a // 32) % 2 == 0:
-        b, c, d, e = b // 64, c // 64, d // 64, e // 64
-        if (not Map[b][d][3][f]) and (not Map[c][e][3][g]):
-            return True
-    else:
-        b, c, d, e = (b + j) // 64, (c + j) // 64, (d + k) // 64, (e + k) // 64
-        if (not Map[b][d][3][h]) and (not Map[c][e][3][i]):
-            return True
-    return False
-
-
 def Game_play(Pressed):
-    global menu, escape
+    global menu, escape, x, y
     if Pressed.get(pygame.K_ESCAPE) and time.time() > escape:
         menu, escape = 1, time.time() + 0.2
         return
-    Player_move(Pressed)
+    x, y = player.Player_move(Pressed, x, y, Map, Width, Length)
     display.display()
 
 
@@ -32,34 +21,3 @@ def Game_menu(Pressed):
     global menu, escape
     if Pressed.get(pygame.K_ESCAPE) and time.time() > escape:
         menu, escape = 0, time.time() + 0.2
-
-
-def Player_move(Pressed):
-    global x, y, velocity
-    move = False
-    if Pressed.get(pygame.K_DOWN) and y // 64 < Width - 1:
-        d = Collision(y + 14, x + 17, x + 50, y + 50, y + 50, 1, 0, 3, 2, 0, -32)
-        if d:
-            y += velocity
-            player.Move("down")
-            move = True
-    elif Pressed.get(pygame.K_UP) and (y + 63) // 64 > 0:
-        d = Collision(y + 16, x + 17, x + 50, y + 4, y + 4, 3, 2, 1, 0, 0, +32)
-        if d:
-            y -= velocity
-            player.Move("up")
-            move = True
-    if Pressed.get(pygame.K_RIGHT) and x // 64 < Length - 1:
-        d = Collision(x + 14, x + 56, x + 56, y + 42, y + 10, 0, 2, 1, 3, -32, 0)
-        if d:
-            x += velocity
-            if not move:
-                player.Move("right")
-    elif Pressed.get(pygame.K_LEFT) and (x + 63) // 64 > 0:
-        d = Collision(x + 16, x + 10, x + 10, y + 42, y + 10, 1, 3, 0, 2, +32, 0)
-        if d:
-            x -= velocity
-            if not move:
-                player.Move("left")
-    elif not move:
-        player.Move("same")
