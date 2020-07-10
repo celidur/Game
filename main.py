80832f6a5d6e7b215e65812b33a5b6ddabb86688
celidur <gaetansjo@gmail.com>
2020-07-10 20:50:24 +0200

    update

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ onclick, running, pressed, pos = False, True, {}, [0, 0]
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT or event.type == pygame.K_ESCAPE:
-            running = False
-            continue
+            if Game.menu == 1 or Game.menu == 2:
+                running = False
+                continue
         elif event.type == pygame.KEYDOWN:
             pressed[event.key] = True
         elif event.type == pygame.KEYUP:
@@ -33,10 +34,10 @@ while running:
                 Game.menu = 0
             elif Game.button_exit.button_clicked(pos[0], pos[1]):
                 running = False
-                Game.save()
                 continue
             elif Game.button_save.button_clicked(pos[0], pos[1]):
                 Game.save()
         else:
             Game.game_menu(pressed)
+Game.save()
 quit()
