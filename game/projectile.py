6ab4a15996f391756efea68204fba6ac5f8f4bc7
celidur <gaetansjo@gmail.com>
2020-10-13 21:21:11 +0200

    update

--- game/projectile.py
+++ game/projectile.py
@@ -4,7 +4,7 @@ import pygame
 class Projectile(pygame.sprite.Sprite):
     def __init__(self, player):
         super().__init__()
-        self.velocity = 10
+        self.velocity = 5
         self.player = player
         self.direction = player.direction
         if self.direction == 'right' or self.direction == 'left':
@@ -26,8 +26,8 @@ class Projectile(pygame.sprite.Sprite):
         else:
             self.rect.x = 350
             self.rect.y = 350
-        self.x = self.rect.x - 320 + self.player.x
-        self.y = self.rect.y - 320 + self.player.y
+        self.x = self.rect.x - 320 + player.x
+        self.y = self.rect.y - 320 + player.y
 
     def mov(self):
         if self.direction == 'right' or self.direction == 'left':
@@ -38,3 +38,5 @@ class Projectile(pygame.sprite.Sprite):
             self.y += self.velocity
             self.rect.x = self.x - self.player.x + 320
             self.rect.y = self.y - self.player.y + 320
+        if self.rect.x < 0 or self.rect.x > 704 or self.rect.y < 0 or self.rect.y > 734:
+            self.player.all_projectiles.remove(self)
